cc7a2ad40024b58a5870745a24f35bfd0d7b2562
Freddy Jimenez <freddy.junior@gmail.com>
2021-02-27 17:41:20 -0600

    adds detail and detail view

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Dog
+from .forms import FeedingForm
 
 def home(request):
     return HttpResponse('<h1> Hello</h1>')
@@ -11,3 +12,12 @@ def about(request):
 def dogs_index(request):
     dogs = Dog.objects.all()
     return render(request, 'dogs/index.html', { 'dogs': dogs })
+
+def dogs_detail(request, cat_id):
+  dog = Dog.objects.get(id=dog_id)
+  toys_dog_doesnt_have = Toy.objects.exclude(id__in = dog.toys.all().values_list('id'))
+  feeding_form = FeedingForm()
+  return render(request, 'dogs/detail.html', {
+    'dog': dog, 'feeding_form': feeding_form,
+    'available_toys': toys_dog_doesnt_have,
+  })
